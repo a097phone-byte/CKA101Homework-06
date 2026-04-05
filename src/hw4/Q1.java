64bd2454f4b634b9a1b812d9a3e6b42b93f6078f
a097phone-byte <a0928826097@gmail.com>
2026-04-05 20:30:12 +0800

    Update Q1.java

--- src/hw4/Q1.java
+++ src/hw4/Q1.java
@@ -5,19 +5,20 @@ package hw4;
 
 public class Q1 {
 	public static void main(String[] args) {
-		int[] x = {29, 100, 39, 41, 50, 8, 66, 77, 95, 15};
-		int sum = 0, avg = 0;
-
+		int sum = 0;
+		int[] x = { 29, 100, 39, 41, 50, 8, 66, 77, 95, 15 };
 		for (int i = 0; i < x.length; i++) {
 			sum += x[i];
-			avg = sum / x.length;
 		}
-
+		int avg = sum / x.length;
+		System.out.println("平均值: " + avg);
+		
+		System.out.print("大於平均值:");
 		for (int i = 0; i < x.length; i++) {
-			if (x[i] > avg) 
+			if (x[i] > avg) {
 				System.out.print(x[i] + " ");
+			}
 		}
-		System.out.println("平均值:" + avg);
 	}
 
 }
